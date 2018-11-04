c097827c1c09d1c078427b5e5ef15feb4c536151
Glitch (harvest-ink) <none>
2018-11-04 22:24:55 +0000

    😾🍖 Updated with Glitch

--- server.js
+++ server.js
@@ -2,8 +2,19 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const util = require('util');
+const dns = require('dns');
+const keyv = require('keyv');
+const k = new keyv();
+
+let currentHash;
+
+(async function(){
+ currentHash = await k.get("lastHash") || 1;
+})();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -12,19 +23,51 @@ app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// TIMESTAMP -- PROJECT 1
+app.get("/api/timestamp/:date", dateEndpoint);
+app.get("/api/timestamp/", dateEndpoint);
+
+function dateEndpoint(req, res) {
+  let date = req.params.date ? new Date(req.params.date) : new Date();
+  if(date == "Invalid Date"){
+    return res.send({"error": "Invalid Date"});
+  } else {
+    return res.send({"unix": date.getTime(), "utc" : date.toUTCString()});
+  }
+}
+
+// URL SHORTENER -- PROJECT 2
+app.post('/api/shorturl/new/', async function(req, res){
+  const URL = req.body.URL;
+  //dns.lookup doesn't seem to be working on Glitch...
+  dns.lookup(URL.replace(/^https?:\/\//gi, ''), async function(err){
+    if(!err){
+      let thisHash = currentHash;
+      currentHash++;
+      await k.set('url-' + thisHash, URL);
+      return res.send({'original_url': URL, 'short_url': thisHash, 'ok': 'yes'});
+    } else {
+      console.log(err);
+      return res.send({'error': 'invalid url'});
+    }
+  });
 });
-
-
+app.get('/api/shorturl/:hash', async function(req, res){
+  let url = await k.get('url-' + req.params.hash);
+  if(url){
+    res.redirect(url);
+  } else {
+    res.redirect('/');
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
